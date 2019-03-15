97b41dd39c2f0795136cff19b5156f6f5b3ac540
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2019-03-15 15:05:59 +0100

    Locally installed HDF5 version working

--- CMakeCPP.cmake
+++ CMakeCPP.cmake
@@ -140,6 +140,20 @@ else()
 endif()
 
 #----------------------------------------------------------------------------
+# HDF5
+#----------------------------------------------------------------------------
+if(NOT STRIDE_FORCE_NO_HDF5)
+    find_package(HDF5 COMPONENTS CXX HL)
+endif()
+if(HDF5_FOUND)
+    include_directories(SYSTEM ${HDF5_CXX_INCLUDE_DIRS})
+else()
+    include_directories(SYSTEM ${CMAKE_HOME_DIRECTORY}/main/resources/lib/hdf5/c++/src)
+    include_directories(SYSTEM ${CMAKE_HOME_DIRECTORY}/main/resources/lib/hdf5/src)
+    include_directories(SYSTEM ${CMAKE_BINARY_DIR}/main/resources/lib/hdf5)
+endif()
+
+#----------------------------------------------------------------------------
 # OpenMP
 #----------------------------------------------------------------------------
 if(NOT STRIDE_FORCE_NO_OPENMP)
