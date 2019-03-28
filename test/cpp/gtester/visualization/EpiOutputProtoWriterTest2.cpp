2c86963af51d4899d4fa5420f117aaf3fe7ff2fa
LaurensVanDamme <laurensvandamme@hotmail.com>
2019-03-28 23:17:49 +0100

    Forgot file

---
+++ test/cpp/gtester/visualization/EpiOutputProtoWriterTest2.cpp
@@ -0,0 +1,45 @@
+/*
+ *  This is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *  The software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with the software. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2019, 2020, Jan Broeckhove and Bistromatics group.
+ */
+
+#include <gtest/gtest.h>
+
+namespace {
+
+TEST(EpiOutputProtoWriterTest, zeroLocationTest)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(EpiOutputProtoWriterTest, oneLocationZeroDaysTest)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(EpiOutputProtoWriterTest, oneLocationOneDayTest)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(EpiOutputProtoWriterTest, oneLocationTwoDaysTest)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(EpiOutputProtoWriterTest, fiveLocationsOneDayTest)
+{
+    EXPECT_TRUE(true);
+}
+
+} // namespace
