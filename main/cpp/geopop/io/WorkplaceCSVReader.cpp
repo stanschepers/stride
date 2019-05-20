e2836f86d0920c8e5812a075ca84820a3c2ef973
Mathias-Ooms <mathias.ooms@student.uantwerpen.be>
2019-05-20 09:52:46 +0200

    travis fixing

--- main/cpp/geopop/io/WorkplaceCSVReader.cpp
+++ main/cpp/geopop/io/WorkplaceCSVReader.cpp
@@ -33,7 +33,6 @@ void WorkplaceCSVReader::SetWorkDistribution(std::vector<std::tuple<double, unsi
 
         for (const CSVRow& row : reader) {
 
-                std::tuple<double, unsigned int, unsigned int> temp;
                 double ratio;
                 unsigned int min_size;
                 unsigned int max_size;
@@ -45,7 +44,7 @@ void WorkplaceCSVReader::SetWorkDistribution(std::vector<std::tuple<double, unsi
                         // Incorrect type
                         throw e; //
                 }
-                temp = {ratio, min_size, max_size};
+                std::tuple<double, unsigned int, unsigned int> temp = {ratio, min_size, max_size};
                 work_distribution.emplace_back(temp);
         }
 }
