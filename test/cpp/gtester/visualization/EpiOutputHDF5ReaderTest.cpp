7e439831006cac267c3ed6f3c83eee97920c7273
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2019-06-17 09:00:00 +0200

    EpiOutputHDF5ReaderTests done

--- test/cpp/gtester/visualization/EpiOutputHDF5ReaderTest.cpp
+++ test/cpp/gtester/visualization/EpiOutputHDF5ReaderTest.cpp
@@ -13,22 +13,197 @@
  *  Copyright 2018, Jan Broeckhove and Bistromatics group.
  */
 
-#include <fstream>
+#include "visualization/source/EpiOutputHDF5Reader.h"
+#include "geopop/GeoGrid.h"
+#include "util/FileSys.h"
+
 #include <gtest/gtest.h>
-#include <memory>
+#include <vector>
 
 namespace {
 
-TEST(EpiOutputHDF5ReaderTest, zeroLocationsTest) { EXPECT_TRUE(true); }
+using namespace std;
+using namespace geopop;
+using namespace stride;
+using namespace stride::util;
+using namespace visualization;
 
-TEST(EpiOutputHDF5ReaderTest, oneLocationZeroDaysTest) { EXPECT_TRUE(true); }
+void getEpiOutputFromJSON(const string& h5File, geopop::GeoGrid<EpiOutput>* epiOutput)
+{
+        EpiOutputHDF5Reader epiOutputHDF5Reader(h5File, epiOutput);
 
-TEST(EpiOutputHDF5ReaderTest, oneLocationOneDayTest) { EXPECT_TRUE(true); }
+        epiOutputHDF5Reader.Read();
+}
 
-TEST(EpiOutputHDF5ReaderTest, twoLocationsFiveDayTest) { EXPECT_TRUE(true); }
+TEST(EpiOutputHDF5ReaderTest, zeroLocationsTest)
+{
+        auto geoGrid = GeoGrid<EpiOutput>(nullptr);
+        getEpiOutputFromJSON(FileSys::GetTestsDir().string() + "/testdata/EpiOutputHDF5/zeroLocationsTest.h5",
+                             &geoGrid);
 
-TEST(EpiOutputHDF5ReaderTest, emptyStreamTest) { EXPECT_TRUE(true); }
+        EXPECT_EQ(0, geoGrid.size());
+}
 
-TEST(EpiOutputHDF5ReaderTest, invalidHDF5Test) { EXPECT_TRUE(true); }
+TEST(EpiOutputHDF5ReaderTest, oneLocationZeroDaysTest)
+{
+        auto geoGrid = GeoGrid<EpiOutput>(nullptr);
+        getEpiOutputFromJSON(FileSys::GetTestsDir().string() + "/testdata/EpiOutputHDF5/zeroDaysTest.h5", &geoGrid);
+
+        EXPECT_EQ(0, geoGrid.size());
+}
+
+TEST(EpiOutputHDF5ReaderTest, oneLocationOneDayTest)
+{
+        auto geoGrid = GeoGrid<EpiOutput>(nullptr);
+        getEpiOutputFromJSON(FileSys::GetTestsDir().string() + "/testdata/EpiOutputHDF5/oneLocationOneDayTest.h5",
+                             &geoGrid);
+
+        EXPECT_EQ(1, geoGrid.size());
+
+        EXPECT_EQ(1, geoGrid[0]->GetID());
+        EXPECT_EQ("test", geoGrid[0]->GetName());
+        EXPECT_EQ(geopop::Coordinate(0,0).get<0>(), geoGrid[0]->GetCoordinate().get<0>());
+        EXPECT_EQ(geopop::Coordinate(0,0).get<1>(), geoGrid[0]->GetCoordinate().get<1>());
+        EXPECT_EQ(1, geoGrid[0]->GetProvince());
+        EXPECT_EQ(0, geoGrid[0]->GetContent()->pop_count);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Susceptible"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Recovered"][0]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Susceptible"][0]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["K12School"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Recovered"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Immune"][0]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Infected"][0]);
+
+        EXPECT_EQ(0.27999999999999997, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Infected"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Recovered"][0]);
+        EXPECT_EQ(0.08, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Immune"][0]);
+
+        EXPECT_EQ(0.12, geoGrid[0]->GetContent()->epiOutput["Senior"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Senior"]["Susceptible"][0]);
+        EXPECT_EQ(0.02, geoGrid[0]->GetContent()->epiOutput["Senior"]["Immune"][0]);
+}
+
+TEST(EpiOutputHDF5ReaderTest, twoLocationsFiveDayTest)
+{
+        auto geoGrid = GeoGrid<EpiOutput>(nullptr);
+        getEpiOutputFromJSON(FileSys::GetTestsDir().string() + "/testdata/EpiOutputHDF5/twoLocationsFiveDaysTest.h5",
+                             &geoGrid);
+
+        EXPECT_EQ(2, geoGrid.size());
+
+        EXPECT_EQ(0, geoGrid[0]->GetID());
+        EXPECT_EQ("test0", geoGrid[0]->GetName());
+        EXPECT_EQ(geopop::Coordinate(0,0).get<0>(), geoGrid[0]->get<0>());
+        EXPECT_EQ(geopop::Coordinate(0,0).get<1>(), geoGrid[0]->get<1>());
+        EXPECT_EQ(0, geoGrid[0]->GetProvince());
+        EXPECT_EQ(0, geoGrid[0]->GetContent()->pop_count);
+
+        EXPECT_EQ(1, geoGrid[1]->GetID());
+        EXPECT_EQ("test1", geoGrid[1]->GetName());
+        EXPECT_EQ(geopop::Coordinate(1.0,1.0).get<0>(), geoGrid[1]->get<0>());
+        EXPECT_EQ(geopop::Coordinate(1.0,1.0).get<1>(), geoGrid[1]->get<1>());
+        EXPECT_EQ(1, geoGrid[1]->GetProvince());
+        EXPECT_EQ(100, geoGrid[1]->GetContent()->pop_count);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Susceptible"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Recovered"][0]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Susceptible"][0]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["K12School"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Recovered"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Immune"][0]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Infected"][0]);
+
+        EXPECT_EQ(0.27999999999999997, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Infected"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Recovered"][0]);
+        EXPECT_EQ(0.08, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Immune"][0]);
+
+        EXPECT_EQ(0.12, geoGrid[0]->GetContent()->epiOutput["Senior"]["Total"][0]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Senior"]["Susceptible"][0]);
+        EXPECT_EQ(0.02, geoGrid[0]->GetContent()->epiOutput["Senior"]["Immune"][0]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Susceptible"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Recovered"][10]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Susceptible"][10]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["K12School"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Recovered"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Immune"][10]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Infected"][10]);
+
+        EXPECT_EQ(0.27999999999999997, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Infected"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Recovered"][10]);
+        EXPECT_EQ(0.08, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Immune"][10]);
+
+        EXPECT_EQ(0.12, geoGrid[0]->GetContent()->epiOutput["Senior"]["Total"][10]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Senior"]["Susceptible"][10]);
+        EXPECT_EQ(0.02, geoGrid[0]->GetContent()->epiOutput["Senior"]["Immune"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Daycare"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Daycare"]["Susceptible"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Daycare"]["Recovered"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["PreSchool"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["PreSchool"]["Susceptible"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["K12School"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["K12School"]["Recovered"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["K12School"]["Immune"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["College"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["College"]["Infected"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Workplace"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Workplace"]["Infected"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Workplace"]["Recovered"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Workplace"]["Immune"][10]);
+
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Senior"]["Total"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Senior"]["Susceptible"][10]);
+        EXPECT_EQ(0, geoGrid[1]->GetContent()->epiOutput["Senior"]["Immune"][10]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Susceptible"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Daycare"]["Recovered"][50]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["PreSchool"]["Susceptible"][50]);
+
+        EXPECT_EQ(0.2, geoGrid[0]->GetContent()->epiOutput["K12School"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Recovered"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["K12School"]["Immune"][50]);
+
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["College"]["Infected"][50]);
+
+        EXPECT_EQ(0.27999999999999997, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Infected"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Recovered"][50]);
+        EXPECT_EQ(0.08, geoGrid[0]->GetContent()->epiOutput["Workplace"]["Immune"][50]);
+
+        EXPECT_EQ(0.12, geoGrid[0]->GetContent()->epiOutput["Senior"]["Total"][50]);
+        EXPECT_EQ(0.1, geoGrid[0]->GetContent()->epiOutput["Senior"]["Susceptible"][50]);
+        EXPECT_EQ(0.02, geoGrid[0]->GetContent()->epiOutput["Senior"]["Immune"][50]);
+}
 
 } // namespace
